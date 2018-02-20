4e5c502fc88f6d27c1b74089fbb4b7a4db58096f
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-02-20 13:14:54 -0800

    Updated travis config and tool

---
+++ tool/travis.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+echo '> pub get ================================================================'
+pub get
+
+if [ -f "package.json" ]; then
+    echo '> npm install ============================================================'
+    npm install
+fi
+
+echo "> pub run test -r expanded ==============================================="
+pub run test -r expanded
+
+echo '> dartfmt -n --set-exit-if-changed . ====================================='
+dartfmt -n --set-exit-if-changed .
+
+echo "> dartanalyzer --fatal-infos --fatal-warnings . =========================="
+dartanalyzer --fatal-infos --fatal-warnings .
