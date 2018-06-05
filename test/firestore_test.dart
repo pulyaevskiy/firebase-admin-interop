755b1b51934064884d33be4a56a3e5470e0b4577
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-06-05 12:52:09 -0700

    Removed unused import

--- test/firestore_test.dart
+++ test/firestore_test.dart
@@ -1,10 +1,9 @@
 // Copyright (c) 2017, Anatoly Pulyaevskiy. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:typed_data';
-
 @TestOn('node')
+import 'dart:async';
+
 import 'package:firebase_admin_interop/firebase_admin_interop.dart';
 import 'package:test/test.dart';
 
