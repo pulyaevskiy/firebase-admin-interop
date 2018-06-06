44ff8f5c14c1fce05d91b25c576bebd7063d65fa
Alexandre Roux Tekartik <alex@tekartik.com>
2018-06-06 18:29:22 +0200

    doc: comments and dartfmt

--- lib/src/firestore_bindings.dart
+++ lib/src/firestore_bindings.dart
@@ -162,23 +162,25 @@ abstract class Transaction {
 }
 
 /// A write batch, used to perform multiple writes as a single atomic unit.
-/// A `WriteBatch` object can be acquired by calling `Firestore.batch()`. It
+///
+/// A [WriteBatch] object can be acquired by calling [Firestore.batch]. It
 /// provides methods for adding writes to the write batch. None of the
-/// writes will be committed (or visible locally) until `WriteBatch.commit()`
+/// writes will be committed (or visible locally) until [WriteBatch.commit]
 /// is called.
+///
 /// Unlike transactions, write batches are persisted offline and therefore are
 /// preferable when you don't need to condition your writes on read data.
 @JS()
 @anonymous
 abstract class WriteBatch {
-  /// Create the document referred to by the provided `DocumentReference`. The
+  /// Create the document referred to by the provided [DocumentReference]. The
   /// operation will fail the batch if a document exists at the specified
   /// location.
   external WriteBatch create(DocumentReference documentRef, DocumentData data);
 
-  /// Write to the document referred to by the provided `DocumentReference`.
+  /// Write to the document referred to by the provided [DocumentReference].
   /// If the document does not exist yet, it will be created. If you pass
-  /// `SetOptions`, the provided data can be merged into the existing document.
+  /// [options], the provided data can be merged into the existing document.
   external WriteBatch set(DocumentReference documentRef, DocumentData data,
       [SetOptions options]);
 
@@ -192,7 +194,6 @@ abstract class WriteBatch {
   external WriteBatch delete(DocumentReference documentRef);
 
   /// Commits all of the writes in this write batch as a single atomic unit.
-  /// successfully written to the backend as an atomic unit.
   external Promise commit();
 }
 
