fd488fb950e20e33aed7498843421721ca525bd9
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-07-25 17:44:51 -0700

    Updated test setup script

--- test/setup.dart
+++ test/setup.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2017, Anatoly Pulyaevskiy. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:convert';
+
 import 'package:node_interop/node.dart';
 import 'package:node_interop/util.dart';
 import 'package:firebase_admin_interop/firebase_admin_interop.dart';
@@ -8,17 +10,18 @@ import 'package:firebase_admin_interop/firebase_admin_interop.dart';
 final Map env = dartify(process.env);
 
 App initFirebaseApp() {
-  if (!env.containsKey('FIREBASE_PROJECT_ID') ||
-      !env.containsKey('FIREBASE_CLIENT_EMAIL') ||
-      !env.containsKey('FIREBASE_PRIVATE_KEY') ||
-      !env.containsKey('FIREBASE_DATABASE_URL'))
+  if (!env.containsKey('FIREBASE_CONFIG') ||
+      !env.containsKey('FIREBASE_SERVICE_ACCOUNT_JSON'))
     throw new StateError('Environment variables are not set.');
 
-  var cert = FirebaseAdmin.instance.cert(
-    projectId: env['FIREBASE_PROJECT_ID'],
-    clientEmail: env['FIREBASE_CLIENT_EMAIL'],
-    privateKey: env['FIREBASE_PRIVATE_KEY'].replaceAll(r'\n', '\n'),
+  Map certConfig = jsonDecode(env['FIREBASE_SERVICE_ACCOUNT_JSON']);
+  final cert = FirebaseAdmin.instance.cert(
+    projectId: certConfig['project_id'],
+    clientEmail: certConfig['client_email'],
+    privateKey: certConfig['private_key'],
   );
-  return FirebaseAdmin.instance.initializeApp(new AppOptions(
-      credential: cert, databaseURL: env['FIREBASE_DATABASE_URL']));
+  final Map config = jsonDecode(env['FIREBASE_CONFIG']);
+  final databaseUrl = config['databaseURL'];
+  return FirebaseAdmin.instance.initializeApp(
+      new AppOptions(credential: cert, databaseURL: databaseUrl));
 }
